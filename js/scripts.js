8b07b9f9cc1788980754749f9815273abb36abc9
Sergei Mikhailenko <sersus1991@gmail.com>
2022-01-04 16:06:59 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -220,32 +220,40 @@
 
 //=======================================================
 
-function basicOp(operation, value1, value2) {
-  switch (operation) {
-    case '+':
-      return value1 + value2;
-  }
+// function basicOp(operation, value1, value2) {
+//   switch (operation) {
+//     case '+':
+//       return value1 + value2;
+//   }
 
-  switch (operation) {
-    case '-':
-      return value1 - value2;
-  }
-  switch (operation) {
-    case '*':
-      return value1 * value2;
-  }
-  switch (operation) {
-    case '/':
-      return value1 / value2;
-  }
-}
+//   switch (operation) {
+//     case '-':
+//       return value1 - value2;
+//   }
+//   switch (operation) {
+//     case '*':
+//       return value1 * value2;
+//   }
+//   switch (operation) {
+//     case '/':
+//       return value1 / value2;
+//   }
+// }
 
-console.log(basicOp('+', 4, 7));
-console.log(basicOp('-', 15, 18));
-console.log(basicOp('*', 5, 5));
-console.log(basicOp('/', 49, 7));
+// console.log(basicOp('+', 4, 7));
+// console.log(basicOp('-', 15, 18));
+// console.log(basicOp('*', 5, 5));
+// console.log(basicOp('/', 49, 7));
 
-//=======================================================
+//========осторожно использовать EVAL(код со строки)========
+// function basicOp(operation, value1, value2) {
+//   return eval(value1 + operation + value2);
+// }
+
+// console.log(basicOp('+', 4, 7));
+// console.log(basicOp('-', 15, 18));
+// console.log(basicOp('*', 5, 5));
+// console.log(basicOp('/', 49, 7));
 //=======================================================
 //=======================================================
 //=======================================================
