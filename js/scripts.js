f814852ef646fe28634fb01f40cd4c5e5a2c35e9
Sergei Mikhailenko <sersus1991@gmail.com>
2022-02-02 19:36:00 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1135,8 +1135,21 @@
 // console.log(duplicateEncode('(( @'));
 // console.log(duplicateEncode('Success'));
 /*
- *=======================================================
+ *=============Multiples of 3 or 5=======================
  */
+// function solution(number) {
+//   let sum = 0;
+
+//   for (let i = 0; i < number; i += 1) {
+//     if (i % 3 === 0 || i % 5 === 0) {
+//       sum += i;
+//     }
+//   }
+
+//   return sum;
+// }
+
+// console.log(solution(10));
 /*
  *=======================================================
  */
