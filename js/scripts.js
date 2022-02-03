06e8156a9fc5a89c5e6ba0261cafd8626de05ab1
Sergei Mikhailenko <sersus1991@gmail.com>
2022-02-03 15:43:03 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1168,6 +1168,25 @@
 /*
  *=======================================================
  */
+// const products = [
+//   { name: 'Radar', price: 1300, quantity: 4 },
+//   { name: 'Scanner', price: 2700, quantity: 3 },
+//   { name: 'Droid', price: 400, quantity: 7 },
+//   { name: 'Grip', price: 1200, quantity: 9 },
+// ];
+
+// function getAllPropValues(propName) {
+//   const arr = [];
+//   for (const product of products) {
+//     if (product.hasOwnProperty(propName)) {
+//       arr.push(product[propName]);
+//     }
+//   }
+
+//   return arr;
+// }
+// console.log(getAllPropValues('price'));
+
 /*
  *=======================================================
  */
