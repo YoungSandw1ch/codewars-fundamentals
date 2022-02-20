a2b89407fffa228620cd97007b7f6ca4a7116a85
YoungSandw1ch <sersus1991@gmail.com>
2022-02-20 12:59:21 +0200

    add regexp js
    complete 5 easy task

---
+++ js/regexp.js
@@ -0,0 +1,159 @@
+/*
+ *====================String cleaning
+ */
+// const stringClean = s => s.replace(/\d/g, '');
+
+// console.log(stringClean('(E3at m2e2!!)')); //(Eat me!!)
+// console.log(
+//   stringClean(
+//     'My "me3ssy" d8ata issues2! Will1 th4ey ever, e3ver be3 so0lved?',
+//   ),
+// );
+/*
+ *====================Get number from string
+ */
+// const getNumberFromString = s => Number(s.replace(/\D/g, ''));
+
+// console.log(getNumberFromString('this is number: 7'));
+// console.log(getNumberFromString('123'));
+/*
+ *====================Regex count lowercase letters
+ */
+// const lowercaseCount = str => str.match(/[a-z]/g)?.length || 0;
+// const lowercaseCount = str => {
+//   if (!str.match(/[a-z]/g)) {
+//     return 0;
+//   }
+//   return str.match(/[a-z]/g).length;
+// };
+// const lowercaseCount = str =>
+//   !str.match(/[a-z]/g) ? 0 : str.match(/[a-z]/g).length;
+
+// console.log(lowercaseCount('abc'));
+// console.log(lowercaseCount("abcABC123!@€£#$%^&*()_-+=}{[]|':;?/>.<,~"));
+// console.log(lowercaseCount("ABC123!@€£#$%^&*()_-+=}{[]|':;?/>.<,~"));
+// console.log(lowercaseCount('abcABC123'));
+/*
+ *====================Simple validation of a username with regex
+ */
+// function validateUsr(username) {
+//   let res = /^[a-z0-9_]{4,16}$/.test(username);
+//   return res;
+// }
+
+// console.log(validateUsr('Hass')); //f
+// console.log(validateUsr('012')); //f
+// console.log(validateUsr('asd43_34')); //t
+// console.log(validateUsr('asd43 34')); //f
+// console.log(validateUsr('asddsa')); //t
+/*
+ *====================
+ */
+
+/*
+ *====================
+ */
+
+/*
+ *====================
+ */
+
+/*
+ *====================
+ */
+
+/*
+ *====================
+ */
+
+/*
+ *====================
+ */
+
+/*
+ *====================
+ */
+
+/*
+ *====================
+ */
+
+/*
+ *====================
+ */
+
+/*
+ *====================
+ */
+
+/*
+ *====================
+ */
+
+/*
+ *====================
+ */
+
+/*
+ *====================
+ */
+
+/*
+ *====================
+ */
+
+/*
+ *====================
+ */
+
+/*
+ *====================
+ */
+
+/*
+ *====================
+ */
+
+/*
+ *====================
+ */
+
+/*
+ *====================
+ */
+
+/*
+ *====================
+ */
+
+/*
+ *====================
+ */
+
+/*
+ *====================
+ */
+
+/*
+ *====================
+ */
+
+/*
+ *====================
+ */
+
+/*
+ *====================
+ */
+
+/*
+ *====================
+ */
+
+/*
+ *====================
+ */
+
+/*
+ *====================
+ */
