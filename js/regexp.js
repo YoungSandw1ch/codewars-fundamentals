a72415aad8af9257c1acc1bba1062c93e5563e8c
Sergei Mikhailenko <sersus1991@gmail.com>
2022-02-21 11:04:43 +0200

    Update regexp.js

--- js/regexp.js
+++ js/regexp.js
@@ -47,29 +47,87 @@
 // console.log(validateUsr('asd43 34')); //f
 // console.log(validateUsr('asddsa')); //t
 /*
- *====================
+ *====================Remove anchor from URL
  */
+// const removeUrlAnchor = url => url.match(/[^\#]+/)[0];
+// const removeUrlAnchor = url => url.split('#')[0];
 
+// console.log(removeUrlAnchor('www.codewars.com/katas/?page=1#about'));
+// console.log(removeUrlAnchor('www.codewars.com/katas/'));
+/*
+ *====================Remove duplicate words
+ */
+// const removeDuplicateWords = s =>
+//   s.split(' ').filter((el, i, a) => a.indexOf(el) === i);
+
+// const removeDuplicateWords = s => [...new Set(s.split(' '))].join(' ');
+
+// console.log(
+//   removeDuplicateWords(
+//     'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta',
+//   ),
+// );
+/*
+ *====================validate code with simple regex
+ */
+// const validateCode = code => /^[1-3]/.test(code);
+
+// console.log(validateCode(9453));
+// console.log(validateCode(248));
+/*
+ *====================Count the number of JavaScript developers coming from Europe
+ */
+// var list1 = [
+//   {
+//     firstName: 'Noah',
+//     lastName: 'M.',
+//     country: 'Switzerland',
+//     continent: 'Europe',
+//     age: 19,
+//     language: 'JavaScript',
+//   },
+//   {
+//     firstName: 'Maia',
+//     lastName: 'S.',
+//     country: 'Tahiti',
+//     continent: 'Oceania',
+//     age: 28,
+//     language: 'JavaScript',
+//   },
+//   {
+//     firstName: 'Shufen',
+//     lastName: 'L.',
+//     country: 'Taiwan',
+//     continent: 'Asia',
+//     age: 35,
+//     language: 'HTML',
+//   },
+//   {
+//     firstName: 'Sumayah',
+//     lastName: 'M.',
+//     country: 'Tajikistan',
+//     continent: 'Asia',
+//     age: 30,
+//     language: 'CSS',
+//   },
+// ];
+
+// const countDevelopers = list =>
+//   list.filter(el => el.continent === 'Europe' && el.language === 'JavaScript')
+//     .length;
+
+// console.log(countDevelopers(list1));
+/*
+ *====================Create Phone Number
+ */
+const createPhoneNumber = numbers => numbers.join('');
+
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+//"(123) 456-7890"
 /*
  *====================
  */
-
-/*
- *====================
- */
-
-/*
- *====================
- */
-
-/*
- *====================
- */
-
-/*
- *====================
- */
-
+// .match(/\(\d{3}\) \d{3}-\d{4}/)
 /*
  *====================
  */
